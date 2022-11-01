0680eacb2515699db77358934dd20e5bf80d8098
Michael Brister <mbrister@new-innov.com>
2022-11-01 13:49:22 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+// NuGet packages: AWSSDK.S3, AWSSDK.SecurityToken, AWSSDK.SSO, AWSSDK.SSOOIDC
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
+
+namespace SSOExample.S3.Programmatic_login
+{
+    class Program
+    {
+        // Requirements:
+        // - An SSO profile in the SSO user's shared config file.
+
+        // Class members.
+        private static string profile = "aws-developers";
+
+        static async Task Main(string[] args)
+        {
+            // Get SSO credentials from the information in the shared config file.
+            var ssoCreds = LoadSsoCredentials(profile);
+
+            // Display the caller's identity.
+            var ssoProfileClient = new AmazonSecurityTokenServiceClient(ssoCreds);
+            Console.WriteLine($"\nSSO Profile:\n {await ssoProfileClient.GetCallerIdentityArn()}");
+
+            // Display a list of the account's S3 buckets.
+            // The S3 client is created using the SSO credentials obtained earlier.
+            var s3Client = new AmazonS3Client(ssoCreds);
+            Console.WriteLine("\nGetting a list of your buckets...");
+            var listResponse = await s3Client.ListBucketsAsync();
+            Console.WriteLine($"Number of buckets: {listResponse.Buckets.Count}");
+            foreach (S3Bucket b in listResponse.Buckets)
+            {
+                Console.WriteLine(b.BucketName);
+            }
+            Console.WriteLine();
+        }
+
+        // Method to get SSO credentials from the information in the shared config file.
+        static AWSCredentials LoadSsoCredentials(string profile)
+        {
+            var chain = new CredentialProfileStoreChain();
+            if (!chain.TryGetAWSCredentials(profile, out var credentials))
+                throw new Exception($"Failed to find the {profile} profile");
+
+            var ssoCredentials = credentials as SSOAWSCredentials;
+
+            ssoCredentials.Options.ClientName = "Example-SSO-App";
+            ssoCredentials.Options.SsoVerificationCallback = args =>
+            {
+                // Launch a browser window that prompts the SSO user to complete an SSO login.
+                //  This method is only invoked if the session doesn't already have a valid SSO token.
+                // NOTE: Process.Start might not support launching a browser on macOS or Linux. If not,
+                //       use an appropriate mechanism on those systems instead.
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = args.VerificationUriComplete,
+                    UseShellExecute = true
+                });
+            };
+
+            return ssoCredentials;
+        }
+
+    }
+
+    // Class to read the caller's identity.
+    public static class Extensions
+    {
+        public static async Task<string> GetCallerIdentityArn(this IAmazonSecurityTokenService stsClient)
+        {
+            var response = await stsClient.GetCallerIdentityAsync(new GetCallerIdentityRequest());
+            return response.Arn;
+        }
+    }
+}
